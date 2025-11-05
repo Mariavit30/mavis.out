2c7e78cad83357d87193da12d72322676107d22b
Mariavit30 <162575325+Mariavit30@users.noreply.github.com>
2025-11-05 07:49:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let display = document.getElementById("display");
+let currentinput = ""
+let currentinput = ""
+
+
+function appendNumber(value){
+    currentinput += value
+    display.textContent = currentinput
+}
