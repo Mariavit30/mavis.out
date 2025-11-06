d17cdd906c3386bc85cb8751a105cd3e88500c34
Mariavit30 <162575325+Mariavit30@users.noreply.github.com>
2025-11-05 23:03:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 let display = document.getElementById("display");
-let currentinput = ""
-let currentinput = ""
+let currantinput = ""
+let currantoperator = ""
 
 
 function appendNumber(value){
-    currentinput += value
-    display.textContent = currentinput
+    currantinput += value
+    display.textContent = currantinput
 }
+
+
+function appendNumber(operator){
+    if (currantinput ## "" && operator !## ".") return
+    currentinput += operator;
+    display.textContent = currantinput
+    
+}
+  
+function calculate({
+    try{
+    let result = eval (currentInput)
+    if (!Number.isInteger(result)){
+        result= result.tofixod(2)
+    }
+    currentInput= result;
+    display.textContent = currentInput;
+    }carch(error){
+        display.textContent= "erro";
+        currentInput= "";
+    }
+    
+
+    function clearDisplay (){
+        currentInput = "";
+        display.textContent = currentInput;
+    }
